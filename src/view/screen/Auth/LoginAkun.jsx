110122ec57091969acd40e8e24320d0f786c212f
ernandarn14 <ernanda.rully@gmail.com>
2020-05-19 22:56:43 +0700

    fix input text

--- src/view/screen/Auth/LoginAkun.jsx
+++ src/view/screen/Auth/LoginAkun.jsx
@@ -13,15 +13,13 @@ class LoginAkun extends React.Component {
             <div className="mt-5">
               <h2>Masuk</h2>
               <h5 className="mt-4">Silahkan masuk dengan nama pengguna</h5>
-              <input type="text" placeholder="Nama Pengguna"  className="mt-4 form-control input-lg" />
-              <br />
+              <input type="text" placeholder="Nama Pengguna"  className="mt-4 form-control form-control-lg" />
               <input
                 type="text"
                 placeholder="Kata Sandi"
-                className="mt-2 form-control input-lg"
+                className="mt-3 form-control form-control-lg"
                 type="password"
               />
-              <br />
               <div className="d-flex justify-content-center">
                 <Button type="contained" className="mt-4">
                   Masuk
--- src/view/screen/Auth/SignupAkun.jsx
+++ src/view/screen/Auth/SignupAkun.jsx
@@ -1,60 +1,73 @@
-import React from 'react';
+import React from "react";
 import "./AuthAkun.css";
 import Button from "../../../component/Button/Buttons.tsx";
 import { Link } from "react-router-dom";
 
-class SignupAkun extends React.Component{
-    render() {
-        return (
-          <div className="d-flex flex-column text-center align-items-center">
-            <div className="row">
-              <div className="col-12 col-lg-offset-6">
-                <div className="mt-5">
-                  <h2>Daftar Akun Baru</h2>
-                  <h5 className="mt-4">
-                    Daftar untuk mendapatkan rekomendasi resep terbaru, membaca artikel, dan membagikan pengalaman baking kamu.
-                  </h5>
-                  <input
-                    type="text"
-                    placeholder="Nama Pengguna"
-                    className="mt-5 form-control input-lg"
-                  /> <br />
-                  <input
-                    type="text"
-                    placeholder="Nama Lengkap"
-                    className="mt-2 form-control"
-                  /><br/>
-                  <input
-                    type="text"
-                    placeholder="Email"
-                    className="mt-2 form-control input-lg"
-                  /><br/>
-                  <input
-                    type="text"
-                    placeholder="Kata Sandi"
-                    className="mt-2 form-control input-lg"
-                  /><br />
-                  <input
-                    type="checkbox"
-                    className="mt-3"
-                    name="showPasswordRegister"
-                  />{" "}
+class SignupAkun extends React.Component {
+  render() {
+    return (
+      <div className="d-flex flex-column text-center align-items-center">
+        <div className="row">
+          <div className="col-12">
+            <div className="mt-5">
+              <h2>Daftar Akun Baru</h2>
+              <h5 className="mt-4">
+                Daftar untuk mendapatkan rekomendasi resep terbaru, membaca
+                artikel, dan membagikan pengalaman baking kamu.
+              </h5>
+              <div className="d-flex justify-content-center">
+                <input
+                  type="text"
+                  placeholder="Nama Pengguna"
+                  className="mt-5 form-control form-control-lg"
+                />
+              </div>
+              <div className="d-flex justify-content-center">
+                <input
+                  type="text"
+                  placeholder="Nama Lengkap"
+                  className="mt-3 form-control form-control-lg"
+                />
+              </div>
+              <div className="d-flex justify-content-center">
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className="mt-3 form-control form-control-lg"
+                />
+              </div>
+              <div className="d-flex justify-content-center">
+                <input
+                  type="password"
+                  placeholder="Kata Sandi"
+                  className="mt-3 form-control form-control-lg"
+                />
+              </div>
+              <div className="d-flex justify-content-center">
+                <input
+                  type="password"
+                  placeholder="Ulang Kata Sandi"
+                  className="mt-3 form-control form-control-lg"
+                />
+              </div>
+              <input
+                type="checkbox"
+                className="mt-3"
+                name="showPasswordRegister"
+              />{" "}
                   Tampilkan Kata Sandi
-                  <br/> 
-                  <div className="d-flex justify-content-center">
-                    <Button
-                      type="contained"
-                      className="mt-4"
-                    >
-                      Daftar
-                    </Button>
-                  </div>
-                </div>
+                  <br />
+              <div className="d-flex justify-content-center">
+                <Button type="contained" className="mt-4">
+                  Daftar
+                </Button>
               </div>
             </div>
           </div>
-        );
-      }
+        </div>
+      </div>
+    );
+  }
 }
 
-export default SignupAkun
+export default SignupAkun;
